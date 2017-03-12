32ce3b21ad62d2b84fa3057abd65b2c5c5b73b5b
Narazaka <info@narazaka.net>
2017-03-12 22:20:28 +0900

    fix

--- src/lib/balloon_surface.ts
+++ src/lib/balloon_surface.ts
@@ -2,7 +2,6 @@ import {ReactiveProperty} from "reactiveproperty";
 import {BalloonData} from "./balloon_data";
 import {BalloonProfile} from "./balloon_profile";
 import {Model} from "./model";
-import {BalloonSurfaceRenderer} from "./renderer/balloon_surface_renderer";
 import {ScopeBalloon} from "./scope_balloon";
 
 export class BalloonSurface implements Model {
@@ -10,7 +9,6 @@ export class BalloonSurface implements Model {
     readonly balloonData: ReactiveProperty<BalloonData>;
     readonly profile: BalloonProfile;
     readonly parent: ScopeBalloon | undefined;
-    renderer: BalloonSurfaceRenderer;
     closed = false;
 
     constructor(
