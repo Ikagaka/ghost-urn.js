9226ef89f3e1080f11f982a8e6a9d1a7554c8d5f
Narazaka <info@narazaka.net>
2017-03-12 22:09:10 +0900

    remove renderer

--- src/lib/renderer/balloon_surface_renderer.ts
+++
@@ -1,8 +0,0 @@
-import {BalloonSurface} from "../balloon_surface";
-import {RendererBase} from "../renderer";
-
-export interface BalloonSurfaceRenderer extends RendererBase {
-    model: BalloonSurface;
-    attachModel(model: BalloonSurface): void;
-    detachModel(): void;
-}
--- src/lib/renderer/shell_surface_renderer.ts
+++
@@ -1,8 +0,0 @@
-import {ShellSurface} from "../shell_surface";
-import {RendererBase} from "../renderer";
-
-export interface ShellSurfaceRenderer extends RendererBase {
-    model: ShellSurface;
-    attachModel(model: ShellSurface): void;
-    detachModel(): void;
-}
